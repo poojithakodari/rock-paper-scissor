153b3b2472efd569ff5cd24722d71ac5972e058b
poojithakodari <136616476+poojithakodari@users.noreply.github.com>
2023-06-18 13:58:40 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,50 +1,34 @@
-m=9
-n=9
-def puzzle(a):
-    for i in range(m):
-        for j in range(m):
-            print(a[i][j],end=" ")
-        print()
-def sudoku(grid,row,col):
-    if row==m-1 and col==m:
-        return True
-    if col==m:
-        row+=1
-        col=0
-    if grid[row][col]>0:
-        return sudoku(grid,row,col+1)
-    for num in range(1,m+1,1):
-        if solve(grid,row,col,num):
-            grid[row][col]=num
-            if sudoku(grid,row,col+1):
-                return True
-        grid[row][col]=0
-    return False
-def solve(grid,row,col,num):
-    for x in range(m):
-        if grid[row][x]==num:
-            return False
-    for x in range(m):
-        if grid[x][col]==num:
-            return False
-    startRow=row-row%3
-    startCol=col-col%3
-    for i in range(3):
-        for j in range(3):
-            if grid[i+startRow][j+startCol]==num:
-                return False
-    return True
-grid=[[2, 5, 0, 0, 3, 0, 9, 0, 1],
-      [0, 1, 0, 0, 0, 4, 0, 0, 0],
-      [4, 0, 7, 0, 0, 0, 2, 0, 8],
-      [0, 0, 5, 2, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 9, 8, 1, 0, 0],
-      [0, 4, 0, 0, 0, 3, 0, 0, 0],
-      [0, 0, 0, 3, 6, 0, 0, 7, 2],
-      [0, 7, 0, 0, 0, 0, 0, 0, 3],
-      [9, 0, 3, 0, 0, 0, 6, 0, 4]]
+# drivers code for project 2
+#rock-paper-scissors
+from random import randint
+choice=["rock","paper","scissor"]
+system_score=user_score=0
+limit=3
+while system_score!=limit and user_score!=limit:
+    print(f"choose among the following:",choice)
+    my_ch=input("player choice:").lower()
+    if my_ch not in choice:
+        print("invalid input")
+        continue
+    sys_ch=choice[int(randint(0,2))]
+    print(f" system choice: {sys_ch}")
+    if my_ch==sys_ch:
+      print("-------match is draw--------")
+    elif my_ch=="rock" and sys_ch=="scissor":
+        user_score+=1
 
-if (sudoku(grid,0,0)):
-   puzzle(grid)
+    elif my_ch=="paper" and sys_ch=="rock":
+        user_score+=1
+    elif my_ch=="scissor" and sys_ch=="paper":
+        user_score+=1
+    else:
+        system_score+=1
+    print(f" your score:{user_score},system score:{system_score}")
+        
+if user_score > system_score:
+    print("you won")
 else:
-    print("no solution")
+    print("system won")
+        
+      
+
